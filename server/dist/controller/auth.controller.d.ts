5a6aa769447dac98a551c9bd638388912ce68707
rahulgope45 <rahuleile456@gmail.com>
2026-03-25 14:08:03 +0530

    added signup controller

--- server/dist/controller/auth.controller.d.ts
+++ server/dist/controller/auth.controller.d.ts
@@ -1,2 +1,3 @@
-export {};
+import type { Request, Response } from 'express';
+export declare const signup: (req: Request, res: Response) => Promise<void>;
 //# sourceMappingURL=auth.controller.d.ts.map
--- server/dist/controller/auth.controller.js
+++ server/dist/controller/auth.controller.js
@@ -1,5 +1,48 @@
+import { prisma } from '../lib/prisma.js';
+import bcrypt from 'bcryptjs';
+import { genrateToken } from '../lib/utils.js';
 //Signup contoller
-export {};
+export const signup = async (req, res) => {
+    try {
+        const { username, email, password, profilepic } = req.body;
+        if (!username || !email || !password) {
+            res.status(401).json({ message: "Please Enter All The Credentials" });
+        }
+        if (password.length < 6) {
+            res.status(401).json({ message: "Please Enter A Password with more than 6 digit" });
+        }
+        //checking user exists or not
+        const user = await prisma.users.findUnique({
+            where: {
+                email: email
+            }
+        });
+        if (user) {
+            res.status(401).json({ message: "User already exists" });
+        }
+        const salt = await bcrypt.genSalt(10);
+        const hashedpassword = await bcrypt.hash(password, salt);
+        const newuser = await prisma.users.create({
+            data: {
+                username,
+                email,
+                password: hashedpassword,
+                profilepic
+            }
+        });
+        if (newuser) {
+            res.status(201).json({
+                message: "User created successfully"
+            });
+        }
+    }
+    catch (error) {
+        console.log(error);
+        res.status(501).json({
+            message: "Internal Server Error"
+        });
+    }
+};
 //Login controller
 //Logout controller
 //checkme
